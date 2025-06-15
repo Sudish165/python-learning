ce5add7b81dfac4c6610d3f37802789db443585c
Sudish Karki <sudishkarki360@gmail.com>
2025-06-15 23:38:29 +0545

    add .py file

---
+++ Listmethod.py
@@ -0,0 +1,25 @@
+# l = [33, 22 ,44 , 2 ,3 , 4]
+# l.sort()      #sano bata thulo value print garxa
+# l.sort(reverse=True)     #thulo bata sano value print garxa
+# l.reverse()       #value ulto print hunxa
+# print(l.count(1))      #count garxa list ma yoo number kati otaa xa vanera
+# print(l.index(44))       # kun index hoo patta lagauxa
+# print(l)
+
+
+# l = [33, 22 ,44 , 2 ,3 , 4]
+# m = l.copy()   #list ko copy banauxa
+# m[0] = 0
+# print(l)
+
+
+# l = [33, 22 ,44 , 2 ,3 , 4]
+# m = [200, 300, 400]
+# k = l + m #2tai list ko index jodxa
+# print(k)
+
+l = [33, 22 ,44 , 2 ,3 , 4]
+l.insert(3, 888)  #jun index halyaa xa teslai replace garxa
+l.extend(l)  #add garxa naya list lai purano list sangaa
+# print(l)
+
